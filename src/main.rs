b57bf6105a28560651d635606c424749e1c7660c
Raunak Gurud <raunakgurud2121@gmail.com>
2023-09-18 17:28:47 +0530

    sending data back to client

--- src/main.rs
+++ src/main.rs
@@ -14,5 +14,9 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
-    println!("Request {}", String::from_utf8_lossy(&buffer[..]))
+    // println!("Request {}", String::from_utf8_lossy(&buffer[..]))
+
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
