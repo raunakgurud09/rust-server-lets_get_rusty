cfbef582893974c03ce843b71e9f3ad0e5b7accd
Raunak Gurud <raunakgurud2121@gmail.com>
2023-09-18 17:51:37 +0530

    simulating a slow request

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::fs;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener: TcpListener = TcpListener::bind("127.0.0.1:4545").unwrap();
@@ -18,23 +20,26 @@ fn handle_connection(mut stream: TcpStream) {
     // println!("Request {}", String::from_utf8_lossy(&buffer[..]))
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
-    let (status_line,filename) = 
-        if buffer.starts_with(get) {
-            ("HTTP/1.1 200 OK","index.html")
-        } else {
-            ("HTTP/1.1 404 NOT FOUND","404.html")
-        };
-    
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
     let contents = fs::read_to_string(filename).unwrap();
-    
+
     let response = format!(
         "{}\r\nContent-Length:{}\r\n\r\n{}",
         status_line,
         contents.len(),
         contents
     );
-    
+
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
